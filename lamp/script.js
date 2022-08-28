ed10a9b1fdfc46f15db27eb32efb1ac1d923dad7
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-08-27 22:26:15 -0300

    Correção de palavra escrita incorretamente

--- lamp/script.js
+++ lamp/script.js
@@ -38,7 +38,7 @@ function setBackgroundBlack() {
 }
 
 function turnOnLamp() {
-    const msg = 'A lãmpada está quebrada, faça a troca para poder ascendê-la';
+    const msg = 'A lãmpada está quebrada, faça a troca para poder acendê-la';
 
     if (isLampBroken) {
         alert(msg);
